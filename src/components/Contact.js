28fee9eb1ca1ece0339bb40e11431fb30a2853db
ugurakcora <uakcora97@gmail.com>
2023-06-20 16:00:08 +0300

    Added Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Banner from "../assets/banner.png";
+import "../styles/Contact.css";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${Banner})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen adınızı soyadınızı giriniz..."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen emailinizi giriniz..."
+          />
+          <label>Mesajınız</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Lütfen mesajınızı giriniz..."
+          ></textarea>
+          <button type="submit">Gönder</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
