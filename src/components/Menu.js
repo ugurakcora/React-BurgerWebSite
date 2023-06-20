202bf9eabbacfa00404df6bbaa464a55aa2d1f48
ugurakcora <uakcora97@gmail.com>
2023-06-20 05:30:02 +0300

    Added Menu Items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { Data } from "../helpers/Data";
+import "../styles/Menu.css";
 const Menu = () => {
-  return <div></div>;
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Burgerlerimiz</h1>
+      <div className="menuList">
+        {Data.map((item) => {
+          const { id, name, image, content, price } = item;
+          return (
+            <div className="menuItem" key={id}>
+              <div
+                className="menuImg"
+                style={{ backgroundImage: `url(${image})` }}
+              ></div>
+              <div className="menuInfo">
+                <div className="menuTitle">
+                  <h2>{name}</h2>
+                  <p>{content}</p>
+                </div>
+                <div className="menuPrice">
+                  <p>{price} TL</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
---
+++ src/helpers/Data.js
@@ -0,0 +1,76 @@
+import React from "react";
+import BurgerImg from "../assets/burger.jpg";
+export const Data = [
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+  {
+    name: "Efso Burger",
+    image: BurgerImg,
+    content: "150 gr kasap burger",
+    price: "₺ 250",
+  },
+];
