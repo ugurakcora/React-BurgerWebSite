27772339b0adf3ea6c45c9fd7df0058dd985cef1
ugurakcora <uakcora97@gmail.com>
2023-06-20 15:48:24 +0300

    Added About

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
 import React from "react";
-
+import BannerYeni from "../assets/banneryeni.jpg";
+import "../styles/About.css";
 const about = () => {
-  return <div></div>;
+  return (
+    <div className="about">
+      <div
+        className="about__top"
+        style={{ backgroundImage: `url(${BannerYeni})` }}
+      ></div>
+      <div className="about__bottom">
+        <h1>HAKKIMIZDA</h1>
+        <p>
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquamlorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default about;
